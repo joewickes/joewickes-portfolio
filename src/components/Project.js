718f161bcf7616cd03c885fc46c06eabe8556d27
Joe Wickes <joe.c.wickes@gmail.com>
2021-02-26 13:52:48 -0500

    Update for semantic HTML and update description

--- src/components/Project.js
+++ src/components/Project.js
@@ -25,9 +25,9 @@ export default class Project extends Component {
       <li className="Project">
         <div className="proj-left">
           <h3>{this.props.title}</h3>
-          <p className="description">{this.props.description}</p>
+          <p className="description"><b>Description: </b>{this.props.description}</p>
           <p><b>Tech Stack Used:</b> {makeTechString(this.props.techUsed)}</p>
-          <a href={this.props.liveLink} rel="noreferrer" target="_blank"><button>See it in Action</button></a>
+          <button><a href={this.props.liveLink} rel="noreferrer" target="_blank">See it in Action</a></button>
           <a href={this.props.frontEndRepo} rel="noreferrer" target="_blank">Client Side Repo</a>
           {this.props.backEndRepo ? (<a href={this.props.backEndRepo} rel="noreferrer" target="_blank">Server Side Repo</a>) : null}
         </div>
